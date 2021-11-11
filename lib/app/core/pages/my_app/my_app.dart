51e6bf51b7a9fe141234b65bbd1463b8a951a699
Pedro Ruas <ruasp0509@gmail.com>
2021-11-10 19:01:39 -0600

    First Steps

---
+++ lib/app/core/pages/my_app/my_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:nyan_app/app/core/pages/my_home_page/my_home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "NYAN Robotics Team",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
